c3df81b917654951e6126b71154b2907496a47fa
Oliver Hope <oliverdnhope@gmail.com>
2018-12-11 20:55:10 +0000

    Doc formatting fixes

--- git_ops.mli
+++ git_ops.mli
@@ -1,38 +1,38 @@
+(** The type of a git store (need to update from ocaml-git*) *)
 type git_store 
-(** The type of a git store (need to update from ocaml-git) **)
 
+(** The type of a git tree *)
 type git_tree
-(** The type of a git tree **)
 
+(** The type of a git hash *)
 type git_hash
-(** The type of a git hash **)
 
+(** perform a git hash-object on given input and store *)
 val hash_object: git_store -> in_channel -> git_hash
-(** perform a git hash-object on given input and store **)
 
+(** perform git cat-file and return contents as an in_channel *)
 val cat_file: git_store -> git_hash -> in_channel
-(** perform git cat-file and return contents as an in_channel **)
 
+(** perform git mktree *)
 val mktree: git_store -> git_tree -> git_hash
-(** perform git mktree **)
 
+(** returns the tree at the HEAD tag of the given repository *)
 val get_head_tree: git_store -> git_tree
-(** returns the tree at the HEAD tag of the given repository **)
 
+(** add the given hash to the given git tree with filename *)
 val add_to_tree: git_tree -> string -> git_hash -> git_tree
-(** add the given hash to the given git tree with filename **)
 
+(** perform git commit_tree operation *)
 val commit_tree: git_store -> git_hash -> git_hash
-(** perform git commit_tree operation **)
 
+(** updates refs/heads/master to the given hash in the git store *)
 val update_head: git_store -> git_hash -> unit
-(** updates refs/heads/master to the given hash in the git store **)
 
+(** convert a string into a git_store type *)
 val store_of_string: string -> git_store
-(** convert a string into a git_store type **)
 
+(** convert a string into a git_tree type *)
 val tree_of_string: string -> git_tree
-(** convert a string into a git_tree type **)
 
+(** convert a string to a git_hash *)
 val hash_of_string: string -> git_hash
-(** convert a string to a git_hash **)
--- maildir.mli
+++ maildir.mli
@@ -1,7 +1,7 @@
 open Core
 
+(** the type of a maildir *)
 type t
-(** the type of a maildir **)
 
+(** delivers a new mail to the specified git store TODO: return something useful *)
 val deliver_mail: Git_ops.git_store -> In_channel.t -> unit
-(** delivers a new mail to the specified git store TODO: return something useful **)
