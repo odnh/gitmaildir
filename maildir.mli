d20ad61cb2e65a4a73a22b4d937eaafa35e529fc
Oliver Hope <oliverdnhope@gmail.com>
2018-12-13 20:19:18 +0000

    Add basic mail delete and move functionality

--- maildir.mli
+++ maildir.mli
@@ -3,5 +3,11 @@ open Core
 (** the type of a maildir *)
 type t
 
-(** delivers a new mail to the specified git store TODO: return something useful *)
+(** delivers a new mail to the specified git store *)
 val deliver_mail: Git_ops.git_store -> In_channel.t -> unit
+
+(** move (ie rename) and email TODO: support having directory hierarchy *)
+val move_mail: Git_ops.git_store -> string -> string -> unit
+
+(** removes the given mail from the current tree TODO: support a directory hierarchy *)
+val delete_mail: Git_ops.git_store -> string -> unit
