52ded631b52a463a8851321673ea6940f56911a5
Oliver Hope <oliverdnhope@gmail.com>
2018-12-17 18:36:27 +0000

    add_to_store and store_of_string complete

--- git_ops.ml
+++ git_ops.ml
@@ -2,8 +2,6 @@ open Core
 open Git_unix
 (*open Lwt.Infix*)
 
-(*type t*)
-
 let store_of_string path =
   Fpath.v path
   |> Store.v
@@ -13,5 +11,3 @@ let add_to_store store input =
   |> Store.Value.Blob.of_string 
   |> Store.Value.blob
   |> Store.write store
-
-
