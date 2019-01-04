f12d5a501bee50cee97a93424d0263c5b4618de4
Oliver Hope <oliverdnhope@gmail.com>
2019-01-04 17:20:51 +0000

    Cleaned up types

--- git_ops.ml
+++ git_ops.ml
@@ -6,8 +6,31 @@ let store_of_string path =
   Fpath.v path
   |> Store.v
 
-let add_to_store store input =
+let add_blob_to_store store input =
   In_channel.input_all input
   |> Store.Value.Blob.of_string 
   |> Store.Value.blob
   |> Store.write store
+
+let get_ref_tree store ref = (* need to work out how to turn a ref into something useful *)
+
+(*let commit_tree store tree message =
+  let author = in (* need to work out how to create a Git.User *)
+  let commiter = in (* same here *)
+  Store.Value.Commit.make ~tree ~author: ~commiter: message*)
+
+let rec add_hash_to_tree tree path hash =
+  let module Tree = Store.Value.Tree in
+  let tree_list = Tree.to_list tree in
+  let mod_mode name path entry =
+    if entry.name = name then
+        let subtree = ... in
+        add_hash_to_tree subtree path hash
+        |> make a hash
+    else
+      entry
+  match Git.Path.segs path with
+  | p::[] -> Tree.entry p `Normal hash |> Tree.add tree
+  | p::ps -> List.map ~f:(mod_node p ps) tree_list
+             |> Tree.of_list
+  | _ ->
--- git_ops.mli
+++ git_ops.mli
@@ -4,7 +4,11 @@ open Git_unix
 val store_of_string : string -> (Store.t, Store.error) result Lwt.t
 
 (** Add new blob to store *)
-val add_to_store : Store.t-> in_channel -> (Digestif.SHA1.t * int, Store.error) result Lwt.t
+val add_blob_to_store : Store.t -> in_channel -> (Store.Hash.t * int, Store.error) result Lwt.t
 
-(** Adds the blob to the tree *)
-(*val add_blob_to_tree : Store.t -> Fpath.t -> Store.Hash.t *)
+(** Get top level git tree for reference *)
+val get_ref_tree : Store.t -> Store.Reference.t -> Store.Hash.t
+
+(** Inserts hash at point given by path in tree *)
+val add_hash_to_tree: Store.t -> Store.Value.Tree.t -> Git.Path.t -> Store.Hash.t
+                      -> (Store.Value.Tree.t, string) result Lwt.t
