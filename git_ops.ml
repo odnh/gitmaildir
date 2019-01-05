08417e788655c8c6c212436de392e0874351b911
Oliver Hope <oliverdnhope@gmail.com>
2019-01-05 18:08:15 +0000

    Remove unnecessary type annotations

--- git_ops.ml
+++ git_ops.ml
@@ -12,20 +12,20 @@ let add_blob_to_store store input =
   |> Store.Value.blob
   |> Store.write store
 
-(*let get_ref_tree store ref = need to work out how to turn a ref into something useful *)
+  (*let get_ref_tree store ref = need to work out how to turn a ref into something useful *)
 
-(*let commit_tree store tree message =
-  let author = in (* need to work out how to create a Git.User *)
-  let commiter = in (* same here *)
-  Store.Value.Commit.make ~tree ~author: ~commiter: message*)
+  (*let commit_tree store tree message =
+    let author = in (* need to work out how to create a Git.User *)
+let commiter = in (* same here *)
+Store.Value.Commit.make ~tree ~author: ~commiter: message*)
 
-(** Returns a pair of a list and the last element in it *)
+  (** Returns a pair of a list and the last element in it *)
 let lwt_option_map a b =
   Lwt.map (fun x -> match x with Some x -> Some (a x) | None -> None) b
 
 let lwt_option_bind a b =
   Lwt.map (fun x -> match x with Some x -> a x | None -> None) b
-  
+
 let (>>>|) a b = lwt_option_map b a
 
 let (>>>=) a b = lwt_option_bind b a
@@ -33,13 +33,13 @@ let (>>>=) a b = lwt_option_bind b a
 let option_pair = function
   | Some a, Some b -> Some (a, b)
   | _ -> None
-  
+
 let get_last l =
   let rev = List.rev l in
   (List.hd rev, Option.map (List.tl rev) ~f:List.rev)
   |> option_pair
 
-(** returns and Ok Tree if is a tree and Error otherwise *)
+  (** returns and Ok Tree if is a tree and Error otherwise *)
 let read_as_tree store hash =
   let data = Store.read store hash in
   let match_tree = function
@@ -61,41 +61,43 @@ let store_write_option store value =
     | Ok (h, _) -> Some h
     | _ -> None)
 
-let add_hash_to_tree (store:Store.t) (tree:Store.Hash.t) (path:Git.Path.t) (hash:Store.Hash.t) : Store.Hash.t option Lwt.t =
+let add_hash_to_tree store tree path hash =
   let module Tree = Store.Value.Tree in
   match get_last @@ Git.Path.segs path with
   | None -> Lwt.return_none
   | Some (name, loc) ->
-    let new_entry = make_tree_entry hash name in
-    let rec aux (path:string list) (tree_hash:Store.Hash.t) : Store.Hash.t option Lwt.t =
-      match path with
+      let new_entry = make_tree_entry hash name in
+      let rec aux path tree_hash =
+        match path with
       | [] ->
-        read_as_tree store tree_hash
+          read_as_tree store tree_hash
         >>>| (fun t -> Tree.add t new_entry)
         >>>| Store.Value.tree
         >>= (fun v -> match v with
           | Some v -> store_write_option store v
           | None -> Lwt.return_none)
-      | x::xs ->
-        let curr_tree = read_as_tree store tree_hash in
-        let subtree_entry = curr_tree >>>= entry_from_tree x in
-        let new_subtree_hash = subtree_entry 
+        | x::xs ->
+            let curr_tree = read_as_tree store tree_hash in
+            let subtree_entry = curr_tree >>>= entry_from_tree x in
+            let new_subtree_hash = subtree_entry 
           >>= (function
-               | Some e -> aux xs e.Tree.node
+            | Some e -> aux xs e.Tree.node
                | None -> Lwt.return_none) in
-        let new_subtree_entry = subtree_entry
+            let new_subtree_entry = subtree_entry
           >>= (fun e -> match e with
             | Some e -> new_subtree_hash >>>| Tree.entry e.Tree.name e.Tree.perm
             | None -> Lwt.return_none) in
-        let new_tree = curr_tree
+            let new_tree = curr_tree
           >>>| Tree.remove ~name:x
           >>= (fun t -> match t with
             | Some t -> new_subtree_entry >>>| Tree.add t
             | None -> Lwt.return None) in
-        new_tree >>>| Store.Value.tree
+            new_tree >>>| Store.Value.tree
           >>= (function
-              | Some v -> Store.write store v >|= (function
-                                                  | Ok (h, _) -> Some h
+            | Some v -> Store.write store v >|= (function
+              | Ok (h, _) -> Some h
                                                   | _ -> None)
-              | None -> Lwt.return_none) in
+            | None -> Lwt.return_none) in
       aux loc tree
+
+
--- git_ops.mli
+++ git_ops.mli
@@ -9,6 +9,6 @@ val add_blob_to_store : Store.t -> in_channel -> (Store.Hash.t * int, Store.erro
 (** Get top level git tree for reference *)
 (*val get_ref_tree : Store.t -> Store.Reference.t -> Store.Hash.t*)
 
-(** Inserts hash at point given by path in tree *)
+(** Inserts hash at point given by path in tree (will fail if intermediate dirs do not exist) *)
 val add_hash_to_tree: Store.t -> Store.Hash.t -> Git.Path.t -> Store.Hash.t
                       -> Store.Hash.t option Lwt.t
