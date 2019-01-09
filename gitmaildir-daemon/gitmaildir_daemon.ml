0440af69ddc3e4281e6257da5c4d7604acf303fe
Oliver Hope <oliverdnhope@gmail.com>
2019-01-09 13:02:54 +0000

    add daemon cli

--- gitmaildir-daemon/gitmaildir_daemon.ml
+++ gitmaildir-daemon/gitmaildir_daemon.ml
@@ -1,1 +1,21 @@
-let () = ()
+open Core
+open Cmdliner
+
+let store_arg =
+  let doc = "Path of the gitmaildir to manage" in
+  let env = Arg.env_var "GITMAILDIR_PATH" ~doc in
+  let doc = "The gitmaildir path" in
+  Arg.(value & opt string "." & info ["d"; "dir"] ~env ~docv:"PATH" ~doc)
+
+let daemon_info =
+  let doc = "Keep a maildir in sync with it's git representation" in
+  let man = [
+    `S Manpage.s_bugs;
+    `P "To report bugs, open an issue at github.com/odnh/gitmaildir" ]
+  in
+  Term.info "gitmaildir_daemon" ~version:"%%VERSION%%" ~doc ~exits:Term.default_exits ~man
+
+let daemon_t =
+    Term.(const run_daemon $ store_arg)
+  
+let () = Term.exit @@ Term.eval (daemon_t, daemon_info)
