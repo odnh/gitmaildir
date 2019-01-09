f7311de6f1b870bb6e73767ae529cabb8fea5670
Oliver Hope <oliverdnhope@gmail.com>
2019-01-09 10:31:07 +0000

    complete basic cli

--- gitmaildir.ml
+++ gitmaildir.ml
@@ -2,6 +2,8 @@ open Core
 open Cmdliner
 open Lwt_result_helpers
 
+(* deliver command *)
+
 let deliver store =
   let store = Git_ops.store_of_string store in
   let deliver_lwt = store >>== (fun s -> Maildir.deliver_mail s In_channel.stdin) in
@@ -13,9 +15,15 @@ let store_arg =
   let doc = "Path of the gitmaildir to deliver to." in
   let env = Arg.env_var "GITMAILDIR_PATH" ~doc in
   let doc = "The gitmaildir path" in
-  Arg.(required & pos 0 (some string) None & info [] ~env ~docv:"PATH" ~doc)
+  Arg.(value & opt string "." & info ["d"; "dir"] ~env ~docv:"PATH" ~doc)
+
+let deliver_info =
+  let doc = "Deliver an email to the gitmaildir" in
+  Term.info "deliver" ~doc ~exits:Term.default_exits
 
 let deliver_t = Term.(const deliver $ store_arg)
+
+(* move command *)
 
 let move store path new_path =
   let store = Git_ops.store_of_string store in
@@ -26,22 +34,54 @@ let move store path new_path =
   | Ok _ -> ()
   | Error _ -> failwith "ERROR"
   
-let path_arg =
+let move_path_arg =
   let doc = "current path of file to move" in
-  Arg.(required & pos 1 (some string) None & info [] ~docv:"MALI_PATH" ~doc)
+  Arg.(required & pos 1 (some string) None & info [] ~docv:"SRC" ~doc)
 
-let new_path_arg =
+let move_new_path_arg =
   let doc = "path to move file to" in
-  Arg.(required & pos 2 (some string) None & info [] ~docv:"NEW_MAIL_PATH" ~doc)
+  Arg.(required & pos 2 (some string) None & info [] ~docv:"DEST" ~doc)
 
-let move_t = Term.(const move $ store_arg $ path_arg $ new_path_arg)
+let move_info =
+  let doc = "Move a file in the gitmaildir" in
+  Term.info "move" ~doc ~exits:Term.default_exits
 
-let info =
-  let doc = "Manage a gitmaildir (git extension to the maildir format)" in
+let move_t = Term.(const move $ store_arg $ move_path_arg $ move_new_path_arg)
+
+(* delete command *)
+
+let delete store path =
+  let store = Git_ops.store_of_string store in
+  let path = Fpath.v path in
+  let delete_lwt = store >>== (fun s -> Maildir.delete_mail s path) in
+  match Lwt_main.run delete_lwt with
+  | Ok _ -> ()
+  | Error _ -> failwith "ERROR"
+  
+let delete_path_arg =
+  let doc = "path of file to delete" in
+  Arg.(required & pos 1 (some string) None & info [] ~docv:"PATH" ~doc)
+
+let delete_info =
+  let doc = "Delete a file in the gitmaildir" in
+  Term.info "delete" ~doc ~exits:Term.default_exits
+
+let delete_t = Term.(const delete $ store_arg $ delete_path_arg)
+
+(* gitmaildir command *)
+
+let gitmaildir_info =
+  let doc = "Manage a gitmaildir" in
   let man = [
     `S Manpage.s_bugs;
     `P "To report bugs, open an issue at github.com/odnh/gitmaildir" ]
   in
   Term.info "gitmaildir" ~version:"0.1" ~doc ~exits:Term.default_exits ~man
 
-let () = Term.exit @@ Term.eval (deliver_t, info)
+let gitmaildir_t =
+  Term.(const ())
+
+let multi_command = Term.eval_choice (gitmaildir_t, gitmaildir_info)
+  [deliver_t, deliver_info; move_t, move_info; delete_t, delete_info]
+
+let () = Term.exit @@ multi_command
