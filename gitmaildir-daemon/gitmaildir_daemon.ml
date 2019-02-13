06dd62fde396d3eb26f184aa9a1b400af7b5a09c
Oliver Hope <oliverdnhope@gmail.com>
2019-02-13 23:06:09 +0000

    Add path argument to gitmaildir_daemon

--- gitmaildir-daemon/gitmaildir_daemon.ml
+++ gitmaildir-daemon/gitmaildir_daemon.ml
@@ -1,16 +1,22 @@
 open Core
 open Cmdliner
 
-let run_daemon _ = ()
+let run_daemon _ _ = ()
 
 let store_arg =
-  let doc = "Path of the gitmaildir to manage" in
+  let doc = "Path of the git store to sync" in
   let env = Arg.env_var "GITMAILDIR_PATH" ~doc in
-  let doc = "The gitmaildir path" in
-  Arg.(value & opt string "." & info ["d"; "dir"] ~env ~docv:"PATH" ~doc)
+  let doc = "The git store path" in
+  Arg.(value & opt string "." & info ["g"; "gitstore"] ~env ~docv:"PATH" ~doc)
+  
+let path_arg =
+  let doc = "Path of the maildir to sync" in
+  let env = Arg.env_var "GITMAILDIR_PATH" ~doc in
+  let doc = "The maildir path" in
+  Arg.(value & opt string "." & info ["m"; "maildir"] ~env ~docv:"PATH" ~doc)
 
 let daemon_info =
-  let doc = "Keep a maildir in sync with it's git representation" in
+  let doc = "Keep a maildir in sync with its git representation" in
   let man = [
     `S Manpage.s_bugs;
     `P "To report bugs, open an issue at github.com/odnh/gitmaildir" ]
@@ -18,6 +24,6 @@ let daemon_info =
   Term.info "gitmaildir_daemon" ~version:"%%VERSION%%" ~doc ~exits:Term.default_exits ~man
 
 let daemon_t =
-    Term.(const run_daemon $ store_arg)
+    Term.(const run_daemon $ store_arg $ path_arg)
 
 let () = Term.exit @@ Term.eval (daemon_t, daemon_info)
