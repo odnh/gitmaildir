84b87765b9f70295f701d6d783e6502730a757cb
Oliver Hope <oliverdnhope@gmail.com>
2019-03-08 12:07:47 +0000

    fix timing output formatting

--- evaluation/scripts/timing_deliver_parallel_params.sh
+++ evaluation/scripts/timing_deliver_parallel_params.sh
@@ -10,5 +10,7 @@ do
 
    { time -p find 1000rand/ -mindepth 1 -print0 | xargs -P$i -n1 -I% -0 sh -c 'cat % | gitmaildir_cli deliver --dir=gmd' ; } 2>&1 | grep real | sed 's/real //' | awk 1 ORS='' >> log.txt
 
+   echo >> log.txt # adds new line
+
    rm -rf gmd
 done
--- evaluation/scripts/timing_deliver_sequential.sh
+++ evaluation/scripts/timing_deliver_sequential.sh
@@ -8,5 +8,7 @@ do
 
    { time -p seq -f "1000rand/file.%04g" 0 $i | tr '\n' '\0' | xargs -P1 -n1 -I% -0 sh -c 'cat % | gitmaildir_cli deliver --dir=gmd' ; } 2>&1 | grep real | sed 's/real //' | awk 1 ORS='' >> log.txt
 
+   echo >> log.txt # adds new line
+
    rm -rf gmd
 done
