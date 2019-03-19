c3744e5e2efdbdb67a8ae71a2930f278528d0a70
Oliver Hope <oliverdnhope@gmail.com>
2019-03-19 21:59:47 +0000

    update mail generation script

--- evaluation/scripts/make_mails.py
+++ evaluation/scripts/make_mails.py
@@ -1,10 +1,16 @@
-import mailbox
+import random
+import string
 from email.mime.text import MIMEText
 
-m = mailbox.Maildir("~/testMail")
-for i in range(1,100000):
-    msg = MIMEText("Email Contents here")
-    msg['Subject'] = "Subj"
+def gen_rand_data(n):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=n))
+
+data_dir = "data_75kB"
+for i in range(10000):
+    msg = MIMEText(gen_rand_data(75000))
+    msg['Subject'] = "Subject"
     msg["From"] = "me"
     msg["To"] = "you"
-    m.add(msg)
+    with open(data_dir + "/file." + str(i).zfill(5), "w+") as f:
+        f.write(msg.as_string())
+        f.write("\n")
