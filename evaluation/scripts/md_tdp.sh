f2b5527a6db24c93982d83ae901fbf84c72e0256
Oliver Hope <oliverdnhope@gmail.com>
2019-03-19 17:26:44 +0000

    Large test script rename and move

--- evaluation/scripts/md_tdp.sh
+++ evaluation/scripts/md_tdp.sh
@@ -3,18 +3,13 @@
 
 for i in `seq 100 100 1000`
 do
-   mkdir md
-   mkdir md/cur md/tmp md/new
-
-   mkdir data
-   seq -f "1000rand/file.%04g" 1 1 $i | xargs cp -t data
+   mkdir gmd
 
    echo -n "$i " >> log.txt
 
-   md_tdp.exe md data log.txt
+   { time -p seq -f "1000rand/file.%04g" 0 $i | tr '\n' '\0' | xargs -P10 -n1 -I% -0 sh -c 'cat % | ../../gitmaildir/_build/default/evaluation/maildir/maildir_cli.exe deliver --dir=gmd' ; } 2>&1 | grep real | sed 's/real //' | awk 1 ORS='' >> log.txt
 
    echo >> log.txt # adds new line
 
-   rm -rf data
-   rm -rf md
+   rm -rf gmd
 done
--- evaluation/scripts/tdp.sh
+++ evaluation/scripts/tdp.sh
@@ -6,15 +6,11 @@ do
    mkdir gmd
    gitmaildir_cli init --dir=gmd
 
-   mkdir data
-   seq -f "1000rand/file.%04g" 1 1 $i | xargs cp -t data
-
    echo -n "$i " >> log.txt
 
-   tdp.exe gmd data log.txt
+   { time -p seq -f "1000rand/file.%04g" 0 $i | tr '\n' '\0' | xargs -P10 -n1 -I% -0 sh -c 'cat % | gitmaildir_cli deliver --dir=gmd' ; } 2>&1 | grep real | sed 's/real //' | awk 1 ORS='' >> log.txt
 
    echo >> log.txt # adds new line
 
-   rm -rf data
    rm -rf gmd
 done
