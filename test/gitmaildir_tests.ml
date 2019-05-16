0ab7efc9ea95f704bae600a3f6862b3e9333fe62
Oliver Hope <oliverdnhope@gmail.com>
2019-05-16 23:25:23 +0100

    Complete tests for git_ops.ml

--- test/gitmaildir_tests.ml
+++ test/gitmaildir_tests.ml
@@ -55,51 +55,102 @@ let add_blob_to_store () =
   run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.add_blob_to_store store ic;
   In_channel.close ic
 
-let commit_tree () = ()
+let get_master_commit () =
+  run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.get_master_commit store
 
-let modify_tree () = ()
+let get_commit_tree () =
+  let hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_master_commit store in
+  run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.get_commit_tree store hash
 
-let add_blob_to_tree () = ()
+let get_commit_parents () =
+  let hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_master_commit store in
+  run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.get_commit_parents store hash
 
-let add_blob_to_tree_extend () = ()
+let get_branch_commit () =
+  run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.get_branch_commit store Git.Reference.(of_string "refs/heads/master")
 
-let remove_entry_from_tree () = ()
+let add_blob_to_tree () =
+  let ic = In_channel.create sample_email in
+  let blob_hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.add_blob_to_store store ic in
+  let commit = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_master_commit store in
+  let tree_hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_commit_tree store commit in
+  In_channel.close ic;
+  run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.add_blob_to_tree store tree_hash Git.Path.(v "mail1") blob_hash 
 
-let make_ref () = ()
+let add_blob_to_tree_extend () =
+  let ic = In_channel.create sample_email in
+  let blob_hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.add_blob_to_store store ic in
+  let commit = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_master_commit store in
+  let tree_hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_commit_tree store commit in
+  In_channel.close ic;
+  run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.add_blob_to_tree_extend store tree_hash Git.Path.(v "new/mail1") blob_hash 
 
-let hash_of_ref () = ()
+let remove_entry_from_tree () =
+  let commit = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_master_commit store in
+  let tree_hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_commit_tree store commit in
+  run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.remove_entry_from_tree store tree_hash Git.Path.(v "mail1")
 
-let update_ref () = ()
+let modify_tree () =
+  let commit = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_master_commit store in
+  let tree_hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_commit_tree store commit in
+  run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.modify_tree ~f:(fun a -> a) store tree_hash Git.Path.(v "") 
 
-let get_master_commit () = ()
+let commit_tree () =
+  let ic = In_channel.create sample_email in
+  let blob_hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.add_blob_to_store store ic in
+  let commit = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_master_commit store in
+  let tree_hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_commit_tree store commit in
+  In_channel.close ic;
+  let new_tree = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.add_blob_to_tree_extend store tree_hash Git.Path.(v "new/mail1") blob_hash in
+  run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.commit_tree store [commit] "Test commit" new_tree
 
-let get_branch_commit () = ()
+let make_ref () =
+  let path = "refs/heads/master" in
+  let gpath = Git.Path.(v path) in
+  Alcotest.(check string) "Ref same as source" (Git_ops.make_ref gpath |> Git.Reference.to_string) path
 
-let get_commit_parents () = ()
+let hash_of_ref () =
+  run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.hash_of_ref store Git.Reference.master
 
-let get_commit_tree () = ()
+let update_ref () =
+  let commit = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_master_commit store in
+  let ref = Git_ops.make_ref Git.Path.(v "refs/heads/master") in
+  let ic = In_channel.create sample_email in
+  let blob_hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.add_blob_to_store store ic in
+  In_channel.close ic;
+  let tree_hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_commit_tree store commit in
+  let new_tree = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.add_blob_to_tree_extend store tree_hash Git.Path.(v "new/mail1") blob_hash in
+  let new_commit = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.commit_tree store [commit] "Test commit" new_tree in
+  run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.update_ref store ref new_commit
 
-let get_hash_at_path () = ()
+let get_hash_at_path () =
+  let commit = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_master_commit store in
+  let tree_hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_commit_tree store commit in
+  run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.get_hash_at_path store tree_hash Git.Path.(v "new/mail1")
 
-let read_blob () = ()
+let read_blob () =
+  let commit = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_master_commit store in
+  let tree_hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_commit_tree store commit in
+  let blob_hash = run_lwt_check_err ~f:(fun a -> a) @@ Git_ops.get_hash_at_path store tree_hash Git.Path.(v "new/mail1") in
+  run_lwt_check_err ~f:(fun _ -> ()) @@ Git_ops.read_blob store blob_hash
 
 let git_ops_set = [
   "init_empty_blob", `Quick, init_empty_blob;
   "add_blob_to_store", `Quick, add_blob_to_store;
-  "commit_tree", `Quick, commit_tree;
-  "modify_tree", `Quick, modify_tree;
+  "get_master_commit", `Quick, get_master_commit;
+  "get_commit_tree", `Quick, get_commit_tree;
+  "get_commit_parents", `Quick, get_commit_parents;
+  "get_branch_commit", `Quick, get_branch_commit;
   "add_blob_to_tree", `Quick, add_blob_to_tree;
   "add_blob_to_tree_extend", `Quick, add_blob_to_tree_extend;
   "remove_entry_from_tree", `Quick, remove_entry_from_tree;
+  "commit_tree", `Quick, commit_tree;
   "make_ref", `Quick, make_ref;
   "hash_of_ref", `Quick, hash_of_ref;
   "update_ref", `Quick, update_ref;
-  "get_master_commit", `Quick, get_master_commit;
-  "get_branch_commit", `Quick, get_branch_commit;
-  "get_commit_parents", `Quick, get_commit_parents;
-  "get_commit_tree", `Quick, get_commit_tree;
   "get_hash_at_path", `Quick, get_hash_at_path;
   "read_blob", `Quick, read_blob;
+  "modify_tree", `Quick, modify_tree;
 ]
 
 (* maildir tests *)
